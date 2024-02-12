2d17c78ac2252da34273e03303c3fdfd5cb6712e
joseplluis <jlribas@girosystem.com>
2024-02-12 12:30:44 +0000

    Password sense números, falla

--- TDDKata.PasswordValidator.Tests/PasswordValidatorTests.cs
+++ TDDKata.PasswordValidator.Tests/PasswordValidatorTests.cs
@@ -5,8 +5,13 @@ namespace TDDKata.PasswordValidator.Tests;
 public class PasswordValidatorTests
 {
     [Fact]
-    public void ValidPassword_Should_Return_True()
+    public void Valid_Password_Should_Return_True()
     {
         PasswordValidator.Validate("1234ABCabc__").Should().Be(true);
+    }
+    
+    [Fact]
+    public void Password_Without_A_Number_Returns_False() {
+        PasswordValidator.Validate("ABCabc__").Should().Be(false);
     }
 }
