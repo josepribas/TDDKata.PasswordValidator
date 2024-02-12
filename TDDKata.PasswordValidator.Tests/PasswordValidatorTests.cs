b6a40cda0a76857d7f0748c93edc6cee5a5eed11
joseplluis <jlribas@girosystem.com>
2024-02-12 12:24:46 +0000

    Password vàlid falla

--- TDDKata.PasswordValidator.Tests/PasswordValidatorTests.cs
+++ TDDKata.PasswordValidator.Tests/PasswordValidatorTests.cs
@@ -3,8 +3,8 @@ namespace TDDKata.PasswordValidator.Tests;
 public class PasswordValidatorTests
 {
     [Fact]
-    public void Test1()
+    public void ValidPassword_Should_Return_True()
     {
-  
+        PasswordValidator.Validate("1234ABCabc__").Should().Be(true);
     }
 }
